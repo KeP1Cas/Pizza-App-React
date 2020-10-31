f4f33c2a9df81dc94b03010337b5a63453c09f49
Danil <daniklucky38@gmail.com>
2020-10-31 20:20:30 +0400

    add: bd.json, PizzaBlock. fix: style-pizza-block

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import Header from "./components/Header/Header";
 import { Route } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect (() => {
+    axios.get('http://localhost:3000/bd.json')
+      .then(({ data }) => {
+        setPizzas(data.pizzas)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact/>
+        <Route path="/" render={() => <Home items={pizzas}/>} exact/>
         <Route path="/cart" component={Cart} exact/>
       </div>
     </div>
